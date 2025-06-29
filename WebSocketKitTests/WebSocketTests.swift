4d9916736e7b4e3c38203b59686409fe8a4c7ea5
doobnet <smirks-quart-0y@icloud.com>
2025-06-29 16:56:09 +0200

    more

--- WebSocketKitTests/WebSocketTests.swift
+++ WebSocketKitTests/WebSocketTests.swift
@@ -8,10 +8,12 @@ struct WebSocketTests {
   class Transport: WebSocketKit.Transport {
     struct Called {
       var resume = 0
+      var cancel: [(closeCode: CloseCode, reason: Data?)] = []
     }
 
     var called = Called()
     var closeCode: CloseCode = .invalid
+    var delegate: (any URLSessionTaskDelegate)?
 
     func receive() async throws -> URLSessionWebSocketTask.Message {
       return .string("test message")
@@ -33,7 +35,7 @@ struct WebSocketTests {
       with closeCode: URLSessionWebSocketTask.CloseCode,
       reason: Data?
     ) {
-
+      called.cancel.append((closeCode, reason))
     }
 
     func resume() {
@@ -51,7 +53,7 @@ struct WebSocketTests {
   func connect_transport() async throws {
     let transport = Transport()
     let webSocket = newWebSocket(transport: transport)
-    
+
     try await webSocket.connect()
 
     #expect(transport.called.resume == 1)
@@ -66,4 +68,30 @@ struct WebSocketTests {
 
     #expect(webSocket.state == .connecting)
   }
+
+  @Test("it disconnects the web socket")
+  func disconnect_transport() async throws {
+    let transport = Transport()
+    let webSocket = newWebSocket(transport: transport)
+
+    try await webSocket.connect()
+    webSocket.disconnect()
+
+    #expect(transport.called.cancel.count == 1)
+    #expect(
+      transport.called.cancel.first! == (closeCode: .normalClosure, reason: nil)
+    )
+  }
+
+  @Test("it updates the state to 'disconnected'")
+  func disconnect_state() async throws {
+    let transport = Transport()
+    let webSocket = newWebSocket(transport: transport)
+
+    try await webSocket.connect()
+    webSocket.disconnect()
+
+    #expect(webSocket.state == .disconnected(closeCode: .normalClosure, closeReason: nil))
+  }
 }
+
